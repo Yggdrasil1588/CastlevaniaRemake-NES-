6bb47f95db9fc6e894950afef09465b2e59eb37b
yggdrasil1588 <j.anderson1588@outlook.com>
2016-03-14 21:34:40 +1000

    14-03-2016

---
+++ Assets/Scripts/Player/PlayerRaycast.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+//Author: J.Anderson
+
+public class PlayerRaycast : MonoBehaviour
+{
+    public RaycastHit playerRaycastOutHitForwards;
+    public RaycastHit playerRaycastOutHitBackwards;
+    public RaycastHit playerRaycastOutHitDown;
+
+    public bool forwardRayIsHitting;
+    public bool backwardRayIsHitting;
+    public bool downRayIsHitting;
+
+    public float forwardDistance;
+    public float backwardDistance;
+    public float downDistance;
+
+    public string backRayTag;
+    public string downRayTag;
+    public string forwardRayTag;
+
+    void Update()
+    {
+        PlayerRaycastOut();
+    }
+
+    void PlayerRaycastOut()
+    {
+
+        Ray forwardsRay = new Ray(gameObject.transform.position, gameObject.transform.forward);
+        if (Physics.Raycast(forwardsRay, out playerRaycastOutHitForwards))
+        {
+            forwardRayIsHitting = true;
+            Debug.DrawRay(gameObject.transform.position, gameObject.transform.forward, Color.red);
+            forwardDistance = playerRaycastOutHitForwards.distance;
+        }
+        else forwardRayIsHitting = false;
+
+        Ray backwardsRay = new Ray(gameObject.transform.position, -gameObject.transform.forward);
+        if (Physics.Raycast(backwardsRay, out playerRaycastOutHitBackwards))
+        {
+            backwardRayIsHitting = true;
+            Debug.DrawRay(gameObject.transform.position, -gameObject.transform.forward, Color.red);
+            backwardDistance = playerRaycastOutHitBackwards.distance;
+        }
+        else
+            backwardRayIsHitting = false;
+
+        Ray downRay = new Ray(gameObject.transform.position, -gameObject.transform.up);
+        if (Physics.Raycast(downRay, out playerRaycastOutHitDown))
+        {
+            Debug.DrawRay(gameObject.transform.position, -gameObject.transform.up, Color.red);
+            downDistance = playerRaycastOutHitDown.distance;
+        }
+    }
+
+}
