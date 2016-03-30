e0a7a7d3df8cd7cd3176406af08d0d387519a636
yggdrasil1588 <j.anderson1588@outlook.com>
2016-03-30 21:21:32 +1000

    30-03-2016

---
+++ Assets/Scripts/Pickups/Destructable.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+//Author: J.Anderson
+
+public class Destructable : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] childObjects;
+    PlayerWeapons playerWeapons;
+    int childCount;
+
+    void Start()
+    {
+        playerWeapons = FindObjectOfType<PlayerWeapons>();
+
+        SetArray();
+    }
+
+    void SetArray()
+    {
+        childCount = gameObject.transform.childCount;
+        childObjects = new GameObject[childCount];
+        for (int i = 0; i < childCount; i++)
+        {
+            childObjects[i] = gameObject.transform.GetChild(i).gameObject;
+        }
+    }
+
+    void SearchArray()
+    {
+        if (playerWeapons.weaponVariables.getPhase1 || playerWeapons.weaponVariables.getPhase2 && !playerWeapons.weaponVariables.getPhase3)
+        {
+            for (int s = 0; s < childCount; s++)
+            {
+                if (childObjects[s].tag == "Whip")
+                {
+                    gameObject.GetComponent<MeshRenderer>().enabled = false;
+                    childObjects[s].SetActive(true);
+                    childObjects[s].GetComponent<Rigidbody>().useGravity = true;
+                    childObjects[s].transform.parent = null;
+                    RebuildArray();
+                }
+            }
+        }
+        else if (playerWeapons.weaponVariables.getPhase3)
+        {
+            for (int s = 0; s < childCount; s++)
+            {
+                if (childObjects[s].tag == "Ammo")
+                {
+                    gameObject.GetComponent<MeshRenderer>().enabled = false;
+                    childObjects[s].SetActive(true);
+                    childObjects[s].GetComponent<Rigidbody>().useGravity = true;
+                    childObjects[s].transform.parent = null;
+                    RebuildArray();
+                }
+            }
+        }
+
+        for (int s = 0; s < childCount; s++)
+        {
+            if (childObjects[s].tag == "Life" || childObjects[s].tag == "Dagger" || childObjects[s].tag == "HolyCross"
+                || childObjects[s].tag == "LargeAmmo" || childObjects[s].tag == "StopWatch" || childObjects[s].tag == "Health")
+            {
+                gameObject.GetComponent<MeshRenderer>().enabled = false;
+                childObjects[s].SetActive(true);
+                childObjects[s].GetComponent<Rigidbody>().useGravity = true;
+                childObjects[s].transform.parent = null;
+                RebuildArray();
+            }
+        }
+    }
+
+    public void RebuildArray()
+    {
+        childCount = gameObject.transform.childCount;
+        childObjects = new GameObject[childCount];
+        for (int i = 0; i < childCount; i++)
+        {
+            childObjects[i] = gameObject.transform.GetChild(i).gameObject;
+        }
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        print(collision);
+        if (collision.tag == "MainWeapon")
+        {
+            SearchArray();
+        }
+    }
+}
---
+++ Assets/Scripts/Pickups/LifePickup.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+//Author: J.Anderson
+
+public class LifePickup : MonoBehaviour
+{
+    PlayerHealthManager playerHealthManager;
+
+    [SerializeField]
+    int addLife;
+
+    void Start()
+    {
+        playerHealthManager = FindObjectOfType<PlayerHealthManager>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerHealthManager.AddLife(addLife);
+            Destroy(gameObject);
+        }
+        if (other.tag == "Ground")
+        {
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+}
